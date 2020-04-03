be2a44f5c425e5ba6e4c3b9e7f049d667799986e
judiw <38564340+judiw@users.noreply.github.com>
2020-04-03 13:31:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,4 +8,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #change defaults feeds
-#sed -i 's/"http://downloads.openwrt.org/snapshots"/"https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/19.07.2"/g' package/base-files/image-config.in
+#sed -i 's/http:\/\/downloads.openwrt\.org\/snapshots/https:\/\/mirrors.tuna.tsinghua.edu.cn\/openwrt\/releases\/19.07.2/g' package/base-files/image-config.in
